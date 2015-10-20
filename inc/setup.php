1a3e7b560595f3ad770099188199b6afc0c0507a
Blaine Robison <brobison@elevatoragency.com>
2015-10-19 17:15:18 -0700

    Use new font-family, "Lato".

--- inc/setup.php
+++ inc/setup.php
@@ -197,7 +197,7 @@ function _s_scripts() {
     if ( ! is_admin() ) {
 
         // Styles
-        wp_enqueue_style( 'googlefonts', add_query_arg( 'family', 'Open+Sans:400,300,700|Roboto:400,100,900italic', '//fonts.googleapis.com/css' ) );
+        wp_enqueue_style( 'googlefonts', add_query_arg( 'family', 'Lato:300,400,700,300italic,400italic,700italic', '//fonts.googleapis.com/css' ) );
         wp_enqueue_style( 'main', get_template_directory_uri() . '/assets/dist/css/main.min.css' );
         wp_enqueue_style( '_s-style', get_stylesheet_uri(), array( 'main' ) );
 
